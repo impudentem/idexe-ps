7bee6992b548f757ef12d39678b7991f26587d24
Aleksandr Goncharenko <info@impudentem.com>
2017-07-08 18:45:33 +0300

    state after install proj

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'idexeonl.mysql.ukraine.com.ua',
+    'database_port' => '',
+    'database_name' => 'idexeonl_ps',
+    'database_user' => 'idexeonl_ps',
+    'database_password' => 'lm3whlfd',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '4DGNN9fh1yxkPi092ugfgDPwFyvJUJGBdUi9Pn2WcsVhU6Wktte8Cykf',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2017-07-08',
+    'locale' => 'ru-RU',
+    'cookie_key' => 'GsVsmTL6ESaGNMOmC86XA5f0MZeWSF41ifiiIHUH5jDrx6m52v69D8qG',
+    'cookie_iv' => 'GBXOU3jZ',
+    'new_cookie_key' => 'def000009f959d37a38b3f2b2e2256a716e28547021c761743147ed414cdda51305f7189826b4782e87050ac6afa148448c0d6bff2aa2d4e542b40899ea720a8c53d7fb8',
+  ),
+);
